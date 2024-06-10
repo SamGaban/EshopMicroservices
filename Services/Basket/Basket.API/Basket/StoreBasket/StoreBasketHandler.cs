dffe201c16375519efa8e2a5db2462b59b5f80fa
Sam G. <gaban.sam@gmail.com>
2024-06-10 11:29:59 +0200

    feat: implemented basket commands/queries/endpoint

---
+++ Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace Basket.API.Basket.StoreBasket;
+
+public record StoreBasketCommand(ShoppingCart Cart) : ICommand<StoreBasketResult>;
+public record StoreBasketResult(string UserName);
+
+public class StoreBasketValidator : AbstractValidator<StoreBasketCommand>
+{
+    public StoreBasketValidator()
+    {
+        RuleFor(x => x.Cart).NotNull().WithMessage("Cart cannot be null");
+        RuleFor(x => x.Cart.UserName).NotEmpty().WithMessage("UserName is required");
+    }
+}
+
+public class StoreBasketCommandHandler : ICommandHandler<StoreBasketCommand, StoreBasketResult>
+{
+    public async Task<StoreBasketResult> Handle(StoreBasketCommand command, CancellationToken cancellationToken)
+    {
+        ShoppingCart cart = command.Cart;
+        
+        // TODO : Store basket to database
+        //await _repository.UpdateBasketAsync(command.Cart);
+
+        return new StoreBasketResult("swn");
+    }
+}
--- Services/Basket/Basket.API/Program.cs
+++ Services/Basket/Basket.API/Program.cs
@@ -1,6 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Retrieving the assembly
+var assembly = typeof(Program).Assembly;
+
+// DI
+
+builder.Services.AddCarter();
+
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+
+var app = builder.Build();
+// HTTP request pipeline
+
+app.MapCarter();
 
 app.Run();
