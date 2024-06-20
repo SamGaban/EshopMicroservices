fafe964ed5e895fbfa935e6e9d92174403ce6375
Sam G. <gaban.sam@gmail.com>
2024-06-20 12:33:25 +0200

    chore: ordering.application basic structure init

---
+++ Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderHandler : ICommandHandler<CreateOrderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        //create order entity from command object
+        //save order entity to database
+        //return result
+        throw new NotImplementedException();
+    }
+}
---
+++ Services/Ordering/Ordering.Application/Orders/Dtos/OrderItemDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ordering.Application.Orders.Dtos;
+
+public record OrderItemDto(
+    Guid Order,
+    Guid ProductId,
+    int Quantity,
+    decimal Price
+    );
