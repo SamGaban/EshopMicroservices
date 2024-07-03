56fa18b058a854f2e5ae6ff0fad891027f8c6310
Sam G. <gaban.sam@gmail.com>
2024-07-03 10:14:41 +0200

    feat: ordering ms cqrs developed

---
+++ Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,24 @@
+﻿using BuildingBlocks.CQRS;
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Extensions;
+using Ordering.Application.Orders.Data;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public class GetOrdersByNameHandler (IApplicationDbContext dbContext)
+    : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResut>
+{
+    public async Task<GetOrdersByNameResut> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await dbContext.Orders
+            .Include(o => o.OrderItems)
+            .AsNoTracking()
+            .Where(o => o.OrderName.Value.Contains(query.Name))
+            .OrderBy(o => o.OrderName)
+            .ToListAsync(cancellationToken);
+
+        var orderDtos = orders.ToOrderDtoList();
+
+        return new GetOrdersByNameResut(orderDtos);
+    }
+}
---
+++ Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQuery.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.CQRS;
+using Ordering.Application.Orders.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName;
+
+public record GetOrdersByNameQuery(string Name) : IQuery<GetOrdersByNameResut>;
+public record GetOrdersByNameResut(IEnumerable<OrderDto> Orders);
