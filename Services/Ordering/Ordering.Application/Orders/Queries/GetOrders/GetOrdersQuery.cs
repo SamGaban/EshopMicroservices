1b3de2bb31f5a388c95664c5020ce2131276d9d2
Sam G. <gaban.sam@gmail.com>
2024-07-03 10:24:26 +0200

    feat: completed pagination with efcore skip/take

---
+++ Services/Ordering/Ordering.Application/Orders/Queries/GetOrders/GetOrdersQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.CQRS;
+using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Dtos;
+
+namespace Ordering.Application.Orders.Queries.GetOrders;
+
+public record GetOrdersQuery(PaginationRequest PaginationRequest) : IQuery<GetOrdersResult>;
+public record GetOrdersResult(PaginatedResult<OrderDto> Orders);
