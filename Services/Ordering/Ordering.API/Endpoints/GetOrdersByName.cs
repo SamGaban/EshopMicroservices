2c951a79a2f8fbcb186a7101610a63a23e9d8b84
Sam G. <gaban.sam@gmail.com>
2024-07-04 10:25:43 +0200

    feat: corrected value object direct access

--- Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
+++ Services/Ordering/Ordering.API/Endpoints/GetOrdersByName.cs
@@ -11,7 +11,7 @@ public class GetOrdersByName : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/orders/{ordersName}", async (string orderName, ISender sender) =>
+        app.MapGet("/orders/{orderName}", async (string orderName, ISender sender) =>
             {
                 var result = await sender.Send(new GetOrdersByNameQuery(orderName));
 
